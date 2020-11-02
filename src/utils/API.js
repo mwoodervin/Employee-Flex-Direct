bea7435f707855d9a1f775695c5a79a0a6d45102
Mary Wood <mwoodervin@gmail.com>
2020-11-01 22:25:30 -0500

    changed search criteria to 8 results

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default {
     getUsers: function() {
-        return axios.get("https://randomuser.me/api/?results=5&nat=us")
+        return axios.get("https://randomuser.me/api/?results=8&nat=us")
     }
 }
