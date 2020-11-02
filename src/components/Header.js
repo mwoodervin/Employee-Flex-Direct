51a985a6bffdb6b088f5136cd91ad409ee917626
Mary Wood <mwoodervin@gmail.com>
2020-11-01 22:24:46 -0500

    styled page header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div>
-            Searchable Employee Information
+        <div className="pageHeader">
+            Employee Information Table
         </div>
     )
 }
